f80eba2ad9319b9e3501cf4bc60e0c89d5226aac
Thomas Lacroix <toto.rigolo@free.fr>
2020-05-03 14:24:01 +0200

    Initial WS support

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,11 @@
+use anyhow::Result;
 use log::*;
-use serde_derive::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use yew::format::Json;
-use yew::services::storage::{Area, StorageService};
 use yew::prelude::*;
+use yew::services::storage::{Area, StorageService};
+use yew::services::websocket::{WebSocketService, WebSocketStatus, WebSocketTask};
 
 const KEY: &str = "cards-client-rs.state";
 
@@ -10,9 +13,51 @@ pub struct App {
     link: ComponentLink<Self>,
     storage: StorageService,
     state: State,
+    ws_service: WebSocketService,
+    ws: WebSocketConnection,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug)]
+pub enum WebSocketConnection {
+    None,
+    Pending(WebSocketTask),
+    Connected(WebSocketTask),
+}
+
+impl WebSocketConnection {
+    fn is_none(&self) -> bool {
+        match &self {
+            Self::None => true,
+            _ => false,
+        }
+    }
+
+    fn is_pending(&self) -> bool {
+        match &self {
+            Self::Pending(_) => true,
+            _ => false,
+        }
+    }
+
+    fn is_connected(&self) -> bool {
+        match &self {
+            Self::Connected(_) => true,
+            _ => false,
+        }
+    }
+
+    fn connected(&mut self) {
+        *self = match std::mem::replace(self, WebSocketConnection::None) {
+            WebSocketConnection::Pending(ws) => WebSocketConnection::Connected(ws),
+            ws => {
+                error!("Ignoring incoherent connected message, status is {:?}.", ws);
+                ws
+            }
+        };
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
 pub struct State {
     value: String,
 }
@@ -20,7 +65,53 @@ pub struct State {
 #[derive(Debug)]
 pub enum Msg {
     Update(String),
+    WebSocket(WsMsg),
+    #[allow(unused)]
+    Ignore,
 }
+
+type AsBinary = bool;
+
+#[derive(Debug)]
+pub enum WsMsg {
+    Close,
+    Closed,
+    Connect,
+    Connected,
+    ErrorOccurred,
+    Received(Result<WsResponse>),
+    Send(AsBinary),
+}
+
+// pub enum Msg {
+//     FetchData(Format, AsBinary),
+//     WsAction(WsAction),
+//     FetchReady(Result<DataFromFile, Error>),
+//     WsReady(Result<WsResponse, Error>),
+//     Ignore,
+// }
+
+impl From<WsMsg> for Msg {
+    fn from(msg: WsMsg) -> Self {
+        Msg::WebSocket(msg)
+    }
+}
+
+/// This type is used as a request which sent to websocket connection.
+#[derive(Serialize, Debug)]
+// struct WsRequest {
+//     #[serde(rename = "type")]
+//     _type: String,
+// }
+pub struct WsRequest(serde_json::Value);
+
+/// This type is an expected response from a websocket connection.
+#[derive(Deserialize, Debug)]
+// pub struct WsResponse {
+//     #[serde(rename = "type")]
+//     _type: String,
+// }
+pub struct WsResponse(serde_json::Value);
 
 impl Component for App {
     type Message = Msg;
@@ -32,11 +123,17 @@ impl Component for App {
         let state = match storage.restore(KEY) {
             Json(Ok(restored_state)) => restored_state,
             _ => State {
-                value: String::from(""),
+                ..Default::default()
             },
         };
 
-        App { link, storage, state }
+        App {
+            link,
+            storage,
+            state,
+            ws_service: WebSocketService::new(),
+            ws: WebSocketConnection::None,
+        }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
@@ -44,11 +141,56 @@ impl Component for App {
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        trace!("Msg: {:?}", msg);
         match msg {
             Msg::Update(val) => {
                 println!("Input: {}", val);
                 self.state.value = val;
             }
+            Msg::WebSocket(action) => match action {
+                WsMsg::Connect => {
+                    let callback = self.link.callback(|Json(data)| WsMsg::Received(data));
+                    let notification = self.link.callback(|status| match status {
+                        WebSocketStatus::Opened => Msg::WebSocket(WsMsg::Connected),
+                        WebSocketStatus::Closed => Msg::WebSocket(WsMsg::Closed),
+                        WebSocketStatus::Error => Msg::WebSocket(WsMsg::ErrorOccurred),
+                    } as Msg);
+                    let task = self
+                        .ws_service
+                        .connect(&self.state.value, callback, notification)
+                        .unwrap();
+                    self.ws = WebSocketConnection::Pending(task);
+                }
+                WsMsg::Connected => {
+                    self.ws.connected();
+                    trace!("{:?}", self.ws);
+                }
+                WsMsg::Send(binary) => {
+                    if let WebSocketConnection::Connected(ws) = &mut self.ws {
+                        let request = WsRequest(json!({"type": "PING"}));
+                        if binary {
+                            ws.send_binary(Json(&request));
+                        } else {
+                            ws.send(Json(&request));
+                        }
+                    } else {
+                        error!("Tried to send on non-opened WS: {:?}", "todo");
+                    }
+                }
+                WsMsg::Received(data) => {
+                    //self.data = data.map(|data| data.value).ok();
+                    info!("Received: {:?}", data);
+                }
+                WsMsg::Close | WsMsg::Closed => {
+                    info!("WebSocket closed.");
+                    self.ws = WebSocketConnection::None;
+                }
+                WsMsg::ErrorOccurred => {
+                    info!("An error occurred on WebSocket.");
+                    self.ws = WebSocketConnection::None;
+                }
+            },
+            Msg::Ignore => return false,
         }
         self.storage.store(KEY, Json(&self.state));
         true
@@ -61,11 +203,30 @@ impl Component for App {
                 { self.view_input() }
                 <br/>
                 { &self.state.value }
+                <br/>
+                <button disabled=!self.ws.is_none()
+                        onclick=self.link.callback(|_| WsMsg::Connect)>
+                    { if self.ws.is_pending() { "Connecting to WebSocket..." } else { "Connect to WebSocket" } }
+                </button>
+                <br/>
+                <button disabled=!self.ws.is_connected()
+                        onclick=self.link.callback(|_| WsMsg::Send(false))>
+                    { "Send To WebSocket" }
+                </button>
+                <br/>
+                <button disabled=!self.ws.is_connected()
+                        onclick=self.link.callback(|_| WsMsg::Send(true))>
+                    { "Send To WebSocket [binary]" }
+                </button>
+                <br/>
+                <button disabled=self.ws.is_none()
+                        onclick=self.link.callback(|_| WsMsg::Close)>
+                    { "Close WebSocket connection" }
+                </button>
             </>
         }
     }
 }
-
 
 impl App {
     fn view_input(&self) -> Html {
@@ -79,5 +240,4 @@ impl App {
     }
 }
 
-impl State {
-}
+impl State {}
